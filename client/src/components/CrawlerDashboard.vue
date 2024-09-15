825b091c36064f80775f344654a4cbd365276010
Fazle Ryan Chowdhury <ryan7998@gmail.com>
2024-09-14 23:08:16 -0400

    update view result

--- client/src/components/CrawlerDashboard.vue
+++ client/src/components/CrawlerDashboard.vue
@@ -45,30 +45,36 @@
                         <table class="w-full text-left">
                         <thead>
                             <tr class="text-sm text-gray-600 uppercase tracking-wide border-b">
-                            <th class="py-2">URL</th>
-                            <th class="py-2">Status</th>
+                                <th class="py-2">URL</th>
+                                <th class="py-2">Status</th>
+                                <th class="py-2"></th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr v-for="(data, url) in crawl.aggregatedData" :key="url" class="text-gray-700 border-b">
-                            <td class="py-2">
-
-                                {{ url }}
-                            </td>
-                            <td class="py-2">
-                                <span v-if="statusDictionary?.[url] === 'started'">
-                                    <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                                    </svg>
-                                </span>
-                                <span v-else
-                                    :class="[
-                                    'inline-block px-2 py-1 rounded-full text-sm',
-                                    ( statusDictionary[url] || data[data.length-1].status ) === 'success' ? 'bg-green-100 text-green-600' : (  statusDictionary[url] || data[data.length-1].status ) === 'failed' ? 'bg-red-100 text-red-600' : 'bg-blue-100 text-blue-600'
-                                ]"
-                                > {{  statusDictionary[url] || data[data.length-1].status  }} </span>
-                            </td>
+                                <td class="py-2">
+                                    {{ url }}
+                                </td>
+                                <td class="py-2">
+                                    <span v-if="statusDictionary?.[url] === 'started'">
+                                        <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                        </svg>
+                                    </span>
+                                    <span v-else
+                                        :class="[
+                                        'inline-block px-2 py-1 rounded-full text-sm',
+                                        ( statusDictionary[url] || data[data.length-1].status ) === 'success' ? 'bg-green-100 text-green-600' : ( statusDictionary[url] || data[data.length-1].status ) === 'failed' ? 'bg-red-100 text-red-600' : 'bg-blue-100 text-blue-600'
+                                    ]"
+                                    > {{ statusDictionary[url] || data[data.length-1].status }} </span>
+                                </td>
+                                <td>
+                                    <a href="#" @click.prevent="openViewResult(url)" >View</a>
+                                    <Teleport to="body">
+                                        <ViewResult v-if="viewResults[url]" :data="data" :url="url"  @close-result-view-event="closeViewResult(url)" :key="url" />
+                                    </Teleport>
+                                </td>
                             </tr>
                         </tbody>
                         </table>
@@ -76,32 +82,6 @@
                 </div>
             </div>
          </div>
-        
-        <!-- Crawl Logs -->
-        <div v-if="logs.length" class="results">
-            <h2>Live logs</h2>
-            <ul>
-                <li v-for="(result, index) in logs" :key="index">
-                    <template v-if="typeof result === 'object'">
-                        <strong>URL:</strong> {{ result.url }} <br />
-                        <strong>Status:</strong> {{ result.status }} <br />
-                        <strong>Data:</strong> {{ result.data ? result.data : 'N/A' }} <br />
-                    </template>
-                    <template v-else>
-                        {{ result ? result : 'N/A' }} <br />
-                    </template>
-                </li>
-            </ul>
-        </div>
-        <div v-if="errors.length" class="errors">
-            <h2>Failed Crawls:</h2>
-            <ul>
-                <li v-for="(error, index) in errors" :key="index">
-                <strong>URL:</strong> {{ error.url }} <br />
-                <strong>Error:</strong> {{ error.message }}
-                </li>
-            </ul>
-        </div>
     </div>
 </template>
 
@@ -110,6 +90,7 @@
     import { io } from "socket.io-client"
     import { useRoute } from 'vue-router'
     import axios from 'axios'
+    import ViewResult from './ViewResult.vue'
 
     const logs = ref([])  // Reactive state for successfull crawl results
     const errors = ref([])  // Reactive state for failed crawl results
@@ -119,7 +100,18 @@
     const crawl = ref(null); // To store crawl data
     const errorMessage = ref(''); // To store any error messages
     const statusDictionary = ref({}) // To store status by listening the socket
+    const showResult = ref(false) // To show result panel
+    const viewResults = ref({})
     
+    const openViewResult = (url) => {
+      // Set the clicked URL to true in the viewResults object
+      viewResults.value = { ...viewResults.value, [url]: true };
+    };
+
+    const closeViewResult = (url) => {
+      // Set the clicked URL to false to close the ViewResult component
+      viewResults.value = { ...viewResults.value, [url]: false };
+    };
     // Function to format time
     const formatTime = (time) => {
     if (!time) return 'N/A';
