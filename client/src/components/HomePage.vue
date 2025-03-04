5e4246d4505e31d40cfa5c5fbd31385653c9f007
Fazle Ryan Chowdhury <ryan7998@gmail.com>
2025-03-04 01:41:12 -0500

    update homepage for staging env

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -51,7 +51,8 @@
     // Fetch crawls from the backend
     const fetchCrawls = async () => {
         try {
-            const response = await axios.get('http://localhost:3001/api/getallcrawlers', {
+            let url = process.env.ORIGIN ?? 'http://localhost:3001'
+            const response = await axios.get(`${url}/api/getallcrawlers`, {
                 params: {
                     page: currentPage.value,
                     limit: limit.value,
