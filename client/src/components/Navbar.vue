466d7f6c6c85879b09c5d4ae0d6ac5b9077e8dc7
ryan7998 <ryan7998@gmail.com>
2025-09-29 21:42:15 +0000

    Fix Navbar.vue build error by moving import.meta.env to computed property

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -7,7 +7,7 @@
                     <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"/>
                     </svg>
-                    <a class="text-xl font-bold text-gray-800 ml-2" href="/">Notify!!</a>
+                    <a class="text-xl font-bold text-gray-800 ml-2" href="/">{{ appName }}</a>
                 </div>
                 <div class="flex items-center space-x-2">
                     <v-btn
@@ -97,7 +97,7 @@
 </template>
 
 <script setup>
-import { ref, inject } from 'vue'
+import { ref, inject, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { useCrawlManagement } from '../composables/useCrawlManagement'
 import CreateCrawlModal from './CreateCrawlModal.vue'
@@ -106,6 +106,11 @@ import QueueStatusModal from './QueueStatusModal.vue'
 
 const router = useRouter()
 const showNotification = inject('showNotification')
+
+// Computed property for app name
+const appName = computed(() => {
+    return import.meta.env.VITE_APP_NAME || 'Notify!!'
+})
 
 // Use the crawl management composable
 const {
